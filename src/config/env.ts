f6728d24bc2f5abfc0fb6991feb6df5d79cc4f8a
Guilherme Nogueira <guilhermenogueira90@gmail.com>
2023-02-18 15:44:12 -0300

    chore(config): modificando arquivo env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,14 +1,7 @@
 const env: NodeJS.ProcessEnv = process.env
 
-const environments: { test: string[]; development: string[] } = {
-  test: ['test', 'testing'],
-  development: ['dev', 'development', 'qa']
-}
-
 export default {
-  isDevelopment: environments.development.includes(String(env.NODE_ENV)),
-  isTesting: environments.test.includes(String(env.NODE_ENV)),
-  debugQueries: [true, 'true'].includes(Boolean(env.DEBUG_QUERIES)),
+  isDevelopment: ['local', 'dev', 'development', 'qa'].includes(String(env.NODE_ENV)),
   envName: env.NODE_ENV,
   appName: env.APP_NAME,
   appVersion: env.APP_VERSION ?? 'local',
